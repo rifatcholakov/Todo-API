7d6f00bf94ae87ba741e8f6c9b9e3a5f5c7fb98e
Rifat Cholakov <rifatcholakov@gmail.com>
2018-07-05 19:56:39 +0300

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
 
-        res.send(todo);
+        res.send({todo});
 
     }, ((e) => res.status(400).send()));
 });
