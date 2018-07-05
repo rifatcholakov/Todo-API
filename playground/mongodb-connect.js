94cf1c77468e29fec952015115457dd170342814
Rifat Cholakov <rifatcholakov@gmail.com>
2018-07-05 17:54:31 +0300

    Add tests for GET /todos

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,34 +1,35 @@
-const { MongoClient, ObjectID } = require('mongodb');
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-    if(err) {
-        return console.log('Unable to connect to MongoDB server');
-    }
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    console.log('Connected to MongoDB server');
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     compleated: false
-    // }, (err, result) => {
-    //     if(err) {
-    //         return console.log('Unable to inert todo', err);
-    //     }
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
-
-    // db.collection('Users').insertOne({
-    //     name: 'Rifat',
-    //     age: 19,
-    //     location: 'Ognyanovo'
-    // }, (err, result) => {
-    //     if(err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-
-    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
-    // });
-
-    db.close();
+  db.close();
 });
--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,38 +1,34 @@
-const { MongoClient, ObjectID } = require('mongodb');
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
-    if(err) {
-        return console.log('Unable to connect to MongoDB server');
-    }
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    console.log('Connected to MongoDB server');
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
-    //deleteMany
-    // db.collection('Users').deleteMany({ name: 'Rifat' }).then((result) => {
-    //     console.log(result);
-    // });
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
-    //findOneAndDelete
-    db.collection('Users').findOneAndDelete({
-        _id: new ObjectID('5b38a79d442c982ff00b6ac1')
-    }).then((result) => {
-        console.log(result);
-    })
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
 
-    //deleteMany
-    // db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then((result) => {
-    //     console.log(result);
-    // });
+  // db.collection('Users').deleteMany({name: 'Andrew'});
 
-    //deleteOne
-    // db.collection('Todos').deleteOne({ text: 'Eat lunch' }).then((result) => {
-    //     console.log(result);
-    // });
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID("57ac8d47878a299e5dc21bc8")
+  }).then((results) => {
+    console.log(JSON.stringify(results, undefined, 2));
+  });
 
-    //findOneAndDelete
-    // db.collection('Todos').findOneAndDelete({ compleated: false }).then((result) => {
-    //     console.log(result);
-    // });
-
-    //db.close();
+  // db.close();
 });
